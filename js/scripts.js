dba57781c3640577a29c6bef85856ba399be04df
CJ Rhone and Randy Weyer <cj@>
2018-04-03 11:21:29 -0700

    Add deposit and withdrawl functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,63 @@
+// business logic
+// var userName = $("#user-name").val();
+// var userInitialDeposit = parseFloat($("#user-initial-deposit").val());
+var newAccount;
+
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+Account.prototype.deposit = function(amount) {
+  return this.balance += amount;
+}
+
+Account.prototype.withdraw = function(amount) {
+  return this.balance -= amount;
+}
 
 
-$(document).ready(function(event) {
 
-  $("#beep-boop-converter").submit(function(event){
+
+
+// user interface logic
+
+
+// <--- Account Creation Form --->
+$(document).ready(function() {
+  $("#bank-account-creation").submit(function(event){
     event.preventDefault();
 
     var userName = $("#user-name").val();
-    var userInitialDeposit = $("#user-initial-deposit").val();
-    var userDeposit = $("#user-deposit").val();
-    var userWithdraw = $("#user-withdraw").val();
+    var userInitialDeposit = parseFloat($("#user-initial-deposit").val());
 
+    newAccount = new Account(userName, userInitialDeposit);
+
+    console.log(newAccount)
+
+    $(".display-balance").text(newAccount.balance);
+    $(".display-name").text(newAccount.name);
+    $(".current-balance").show();
 
   });
+
+
+// <--- Transfer Form --->
+  $("#bank-account-transfer").submit(function(event){
+    event.preventDefault();
+
+    var userDeposit = parseFloat($("#user-deposit").val());
+    var userWithdraw = parseFloat($("#user-withdraw").val());
+
+    // newAccount.balance.Withdraw(userWithdraw);
+    newAccount.deposit(userDeposit);
+    newAccount.withdraw(userWithdraw);
+
+
+    $(".display-balance").text(newAccount.balance);
+    $(".display-name").text(newAccount.name);
+
+    console.log(newAccount)
+  });
+
 });
